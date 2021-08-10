0c863253ce8031c6425252a7fc911028e7c575cc
castilla-aurela <lfcastilla@gmail.com>
2021-08-09 20:04:16 -0500

    views and url

--- usersProject/urls.py
+++ usersProject/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from usersApp.views import VerifyTokenView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+    path('token/verify/', VerifyTokenView.as_view())
 ]
